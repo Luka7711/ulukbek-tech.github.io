61b461bc940e123e026d29f73aba9f5318ad885a
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-07-19 11:51:34 -0500

    large size screens

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
-// let links = document.getElementsByClassName("nav-link");
-// let scrollTo = [".about", ".section-exp", ".container-project", ".container-submit"];
+let links = document.getElementsByClassName("nav-link");
+let scrollTo = [".about", ".section-exp", ".container-project", ".container-submit"];
 
 
-// for (let i = 0; i < links.length; i++) {
-// 	links[i].addEventListener("click", () => {
-// 			let x = window.scrollY + document.querySelector(scrollTo[i]).getBoundingClientRect().top // Y
-// 			let y = window.scrollX + document.querySelector(scrollTo[i]).getBoundingClientRect().left
-// 			window.scrollTo(y, x);
-// 	});
-// }
+for (let i = 0; i < links.length; i++) {
+	links[i].addEventListener("click", () => {
+			let x = window.scrollY + document.querySelector(scrollTo[i]).getBoundingClientRect().top // Y
+			let y = window.scrollX + document.querySelector(scrollTo[i]).getBoundingClientRect().left
+			window.scrollTo(y, x);
+	});
+}
 
 let hamburger = document.querySelector(".hamburger");
 let navbar = document.querySelector(".nav-bar");
