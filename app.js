52334191d41e2a3b808b0b9b0a69fe3d4e35fd97
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-07-20 15:18:54 -0500

    images replaced to icons

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-let links = document.getElementsByClassName("nav-link");
+let links = document.getElementsByClassName("scroll-link");
 let scrollTo = [".about", ".section-exp", ".container-project", ".container-submit"];
 
 
 for (let i = 0; i < links.length; i++) {
 	links[i].addEventListener("click", () => {
-			let x = window.scrollY + document.querySelector(scrollTo[i]).getBoundingClientRect().top // Y
-			let y = window.scrollX + document.querySelector(scrollTo[i]).getBoundingClientRect().left
+		let x = window.scrollY + document.querySelector(scrollTo[i]).getBoundingClientRect().top // Y
+		let y = window.scrollX + document.querySelector(scrollTo[i]).getBoundingClientRect().left
 			window.scrollTo(y, x);
 	});
 }
