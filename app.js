2a216396cfc1ab467f941f19602b8d14ab0697c1
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-07-27 22:44:42 -0500

    font family changed:

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
+window.onload = () => {
+	let links = document.getElementsByClassName("scroll-link");
+	let scrollTo = [".about", ".section-exp", ".container-project", ".container-submit"];
 
-let links = document.getElementsByClassName("scroll-link");
-let scrollTo = [".about", ".section-exp", ".container-project", ".container-submit"];
+	for (let i = 0; i < links.length; i++) {
+		links[i].addEventListener("click", () => {
+			let x = window.scrollY + document.querySelector(scrollTo[i]).getBoundingClientRect().top // Y
+			let y = window.scrollX + document.querySelector(scrollTo[i]).getBoundingClientRect().left
+				window.scrollTo(y, x);
+		});
+	}
+
+	let hamburger = document.querySelector(".hamburger");
+	let navbar = document.querySelector(".nav-bar");
 
 
-for (let i = 0; i < links.length; i++) {
-	links[i].addEventListener("click", () => {
-		let x = window.scrollY + document.querySelector(scrollTo[i]).getBoundingClientRect().top // Y
-		let y = window.scrollX + document.querySelector(scrollTo[i]).getBoundingClientRect().left
-			window.scrollTo(y, x);
+	hamburger.addEventListener('click', () => {
+		navbar.classList.toggle("active");
+		hamburger.classList.toggle("active");
+		console.log("ha")
 	});
-}
 
-let hamburger = document.querySelector(".hamburger");
-let navbar = document.querySelector(".nav-bar");
+	function styleBulletList() {
+		let ul = document.querySelectorAll('.duty-container');
+		ul.forEach(ul => {
+				ul.children.forEach(li => console.log(li))
+		});
+	}
 
+	styleBulletList()
 
-hamburger.addEventListener('click', () => {
-	navbar.classList.toggle("active");
-	hamburger.classList.toggle("active");
-});
+};
